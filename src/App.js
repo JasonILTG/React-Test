b2a3d30611c760270904a7a13a679468bf1ad75a
Jason <jasoniltg@gmail.com>
2016-04-10 13:06:12 -0700

    Figured Out React-Routing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import React from 'react';
-import BulletContainer from './BulletContainer.js';
+import { Route, Router, hashHistory } from 'react-router';
+
+import BulletApp from './BulletApp.js';
 
 /* Main app class */
 class App extends React.Component {
-	constructor() {
-		super();
-
-		// Set initial nextID to 1
-		this.state = {
-			nextID: 1
-		}
-	}
-
-	/* Returns the next unused ID (for component keys) */
-	getNextID() {
-		var toReturn = this.state.nextID;
-		this.setState({nextID: this.state.nextID + 1});
-		return toReturn;
-	}
-
 	render() {
 		return (
 			<div>
-				<h1>Bullet Points</h1>
-
-				{/* BulletContainer to contain all bullets */}
-				<BulletContainer getNextID={this.getNextID.bind(this)} />
+				<h1>Navigation</h1>
+				<a href="#/bullet">Bullet App</a>
 			</div>
 		)
 	}
 }
 
+let routes = (
+	<Router history={hashHistory}>
+		<Route path="/" component={App} />
+		<Route path="/bullet" component={BulletApp} />
+	</Router>
+);
+
 // Render the app
-React.render(<App />, document.getElementById('app'));
+React.render(routes, document.getElementById('app'));
---
+++ src/BulletApp.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import BulletContainer from './BulletContainer.js';
+
+/* Main app class */
+class BulletApp extends React.Component {
+	constructor() {
+		super();
+
+		// Set initial nextID to 1
+		this.state = {
+			nextID: 1
+		}
+	}
+
+	/* Returns the next unused ID (for component keys) */
+	getNextID() {
+		var toReturn = this.state.nextID;
+		this.setState({nextID: this.state.nextID + 1});
+		return toReturn;
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Bullet Points</h1>
+
+				{/* BulletContainer to contain all bullets */}
+				<BulletContainer getNextID={this.getNextID.bind(this)} />
+			</div>
+		)
+	}
+}
+
+export default BulletApp;
